49d692468092ba1433ec61a1d0b9c242c97cb43a
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2022-09-22 22:27:40 +0900

    #06 Icon with react-icons and Header with GODkra

--- README.md
+++ README.md
@@ -30,4 +30,13 @@ npm i react-router-dom
 
 #### #04 ChakraProvider and React Router Setting
 
+- v6의 React Router는 좀 다른데, 어떤 특정 path안에 Root 컴포넌트와 children 컴포넌트를 같이 넣을 수 있고, 그렇게 넣으면
+  Outlet이라는 컴포넌트를 사용하여 children들을 뿌릴수가 있다. path에 따라 뿌려지는 children들이 달라지는데 router.tsx, Root.tsx를 참고
+
 #### #05 NotFound page with Chakra G.O.D
+
+#### #06 Icon with react-icons and Header with GODkra
+
+```bash
+npm i react-icons --save
+```
--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,10 +1,27 @@
+import { Box, Button, HStack, Text } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
-
+import { FaAirbnb } from 'react-icons/fa';
 export default function Root() {
   return (
-    <>
-      <h1>Im root</h1>
+    <Box>
+      <HStack
+        py={5}
+        px={'24'}
+        borderBottomWidth={1}
+        justifyContent={'space-between'}
+      >
+        <HStack>
+          <Box color={'red.500'} alignItems={'center'}>
+            <FaAirbnb size={38} />
+          </Box>
+          <Text color={'red.500'}>Chyoneebnb</Text>
+        </HStack>
+        <HStack>
+          <Button>Log in</Button>
+          <Button>Sign up</Button>
+        </HStack>
+      </HStack>
       <Outlet />
-    </>
+    </Box>
   );
 }
